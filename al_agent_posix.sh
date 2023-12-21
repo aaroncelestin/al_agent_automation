76472b64cabde089615e66d89e327b83b489b8c3
Aaron Celestin <51392307+aaroncelestin@users.noreply.github.com>
2023-12-20 18:59:20 -0600

    Update al_agent_posix.sh
    Added support for AWS Linux 2, 2023, and 2022
    Automatic install of rsyslog for distros shipped without it (AWS Linux 2022/3, Centos, Fedora, etc)
    Improved fault tolerance of SELinux configurations

--- al_agent_posix.sh
+++ al_agent_posix.sh
@@ -66,6 +66,15 @@ syslog_conf_file='/etc/rsyslog.conf'
 rsyslogng_text='destination d_alertlogic {tcp("localhost" port(1514));};
 log { source(s_sys); destination(d_alertlogic); };'
 rsyslog_text='*.* @@127.0.0.1:1514;RSYSLOG_FileFormat'
+agent_docs_url='https://docs.alertlogic.com/requirements/agent.htm?tocpath=Requirements%7C_____4'
+journ_conf_file='/etc/systemd/journald.conf'
+fts_yes='ForwardToSyslog=yes'
+semx_none='Port tcp/1514 is not defined'
+semx_exists='Port tcp/1514 already defined'
+err_no_file='no such file or directory'
+err_no_cmd='command not found'
+als_no_run='al-agent is NOT running.'
+als_is_run='al-agent is running with pid'
 
 # Packages will be linked but only downloaded when the agent is ready to be installed.
 deb32='https://scc.alertlogic.net/software/al-agent_LATEST_i386.deb'
@@ -155,7 +164,40 @@ configure_agent () {
 # helper functions to check if the syslog conf has been modified
 _check_rsyslog () { grep "$rsyslog_text" "$syslog_conf_file" 2>/dev/null; }
 _check_rsyslog_ng () { grep "$rsyslogng_text" "$syslogng_conf_file" 2>/dev/null; }
+_yum_install_rsyslog () { sudo yum install rsyslog -y && sudo systemctl enable rsyslog && sudo systemctl start rsyslog; }
 
+_os_is_aws_linux ()
+{
+    local nix_id=$(grep -r "\bID\b" /etc/os-release | cut -d= -f2 | sed 's/\"//g' 2>/dev/null)
+    local nix_vers=$(grep -r "\bVERSION\b" /etc/os-release | cut -d= -f2 | sed 's/\"//g' 2>/dev/null)
+    if [ "$nix_id" = "amzn" ]; then 
+        if [ "$nix_vers" = "2022" ] || [ "$nix_vers" = "2023" ] || [ "$nix_vers" = "2" ]; then
+            echo true
+        fi
+    else
+        echo false
+    fi
+}
+
+_configure_journald () 
+{
+    if [ -f "$journ_conf_file" ]; then 
+        ftsline=$(grep -n 'ForwardToSyslog' "$journ_conf_file")                             # returns 35:#ForwardToSyslog=no
+        [ -n "$ftsline" ] && { line_num=$(echo "$ftsline" | cut -d: -f1); }                 # returns 35 (line number)
+        [ -n "$line_num" ] && { set_fts=$(echo "$ftsline" | cut -d: -f2 | cut -d= -f2); }   # returns no|yes
+        if [ "$set_fts" = 'yes' ]; then                                                     
+            sudo sed -i "${line_num} s/^##*//" "$journ_conf_file"                                # just uncomment line if set_fts was 'yes'
+        elif [ "$set_fts" = 'no' ] && [ -n "$line_num" ]; then
+            sudo sed -i "${line_num} s/.*/${fts_yes}/" "$journ_conf_file"                        # if set_fts was 'no' replace line with fts_yes 
+        else
+            echo "ERROR: failed to update journald config file, no matching rsyslog forwarding rules were found in $journ_conf_file"
+            return 13
+        fi
+    else
+        echo "ERROR: journald config file $journ_conf_file was not found"
+        return 14
+    fi
+}
 
 # Configure SYSLOG Collection
 make_syslog_config () {
@@ -183,47 +225,73 @@ make_syslog_config () {
         elif [ -n "$(_check_rsyslog)" ]; then
             echo "WARNING: rsyslog was already configured. No changes were made to $syslog_conf_file"
         fi
-    else    
-        echo "ERROR: No rsyslog configuration file was found. Please configure syslog manually."; exit 1
+    elif $(_os_is_aws_linux) && [ ! -f "$syslog_conf_file" ] && [ ! -f "$syslogng_conf_file" ]; then
+        echo "INFO: Host is Amazon Linux $(grep -r "\bVERSION\b" /etc/os-release | cut -d= -f2 | sed 's/\"//g' 2>/dev/null)"
+        echo "INFO: Installing rsyslog from Centos/RHEL repos..."
+        sleep 2s
+        if _yum_install_rsyslog ; then
+            echo "INFO: Modifying journald to forward logs to rsyslog. Please wait..."
+            sleep 2s
+            if _configure_journald ; then 
+                echo "Configuring rsyslog to forward all logs to AlertLogic agent..."
+                sleep 2s
+                make_syslog_config # recurse once to config rsyslog after its been installed
+            else
+                echo "ERROR: journald configuration failed"
+                return 15
+            fi
+        else
+            echo "ERROR: rsyslog installation failed"
+            return 16
+        fi    
+    else
+        echo 'ERROR: Linux Distro could not be determined and rsyslog configuration file was not found.'
+        echo "Check to ensure this distro is supported by AlertLogic Agent here: $agent_docs_url"
+        echo 'If this distro is supported, then you may have to configure rsyslog manually.'
+        exit 1
     fi
 }
 
 check_enforce () {
     if [ -z "$(which getenforce 2>&1)" ]; then
         echo "INFO: SELinux is not enabled. Semanage utils will not be installed."
-    elif [ -n "$(which getenforce 2>&1)" ] && [ "$(getenforce 2>&1)" = 'Disabled' ]; then
-        echo "INFO: SELinux is enabled but getenforce is disabled. Semanage utils will not be installed."
-    elif [ -n "$(which getenforce 2>&1)" ] && [ -z "$(which semanage 2>&1)" ]; then
-            echo "WARNING: SELinux is enabled but semanage is not available."
-            echo "INFO: Installing semanage with policycoreutils python utils package..."       
-        if [ -n "$(command -v apt 2>&1)" ]; then
-            echo "INFO: using apt to install policycoreutils..."
-            sudo apt install policycoreutils-python-utils -y
-        elif [ -n "$(command -v zypper 2>&1)" ]; then
-            echo "INFO: using zypper to install policycoreutils..."
-            sudo zypper install --no-confirm policycoreutils-python-utils
-        elif [ -n "$(command -v yum 2>&1)" ]; then
-            echo "INFO: using yum to install policycoreutils..."
-            sudo yum install policycoreutils-python-utils -y
+    elif [ -n "$(which getenforce 2>&1)" ]; then
+        if [ "$(getenforce 2>&1)" = 'Disabled' ] || [ "$(getenforce 2>&1)" = 'Permissive' ]; then
+            echo "INFO: SELinux is enabled but is Disabled or Permissive. Semanage utils will not be installed."
+        elif [ "$(getenforce 2>&1)" = 'Enforcing' ]; then
+            if [ -z "$(which semanage 2>&1)" ]; then
+                echo "WARNING: SELinux is enforcing but semanage tools are not available. Semanage tools must be installed for port configuration."
+                echo "INFO: Installing semanage with policycoreutils python utils package..."       
+                if [ -n "$(command -v apt 2>&1)" ]; then
+                    echo "INFO: using apt to install policycoreutils..."
+                    sudo apt install policycoreutils-python-utils -y
+                elif [ -n "$(command -v zypper 2>&1)" ]; then
+                    echo "INFO: using zypper to install policycoreutils..."
+                    sudo zypper install --no-confirm policycoreutils-python-utils
+                elif [ -n "$(command -v yum 2>&1)" ]; then
+                    echo "INFO: using yum to install policycoreutils..."
+                    sudo yum install policycoreutils-python-utils -y
+                fi
+                check_enforce # run this function again after semanage tools install
+            elif [ -n "$(which semanage 2>&1)" ]; then
+                echo "INFO: SELinux is enabled and in enforcing mode and semanage tools are available."
+                echo "INFO: Checking if port exclusion rule has already been added..."
+                sleep 1s
+                semx_stat=$(sudo semanage port -a -t syslogd_port_t -p tcp 1514 2>&1)
+                if [ -n "$(echo "$semx_stat" | grep -F "$semx_exists")" ]; then
+                    echo "INFO: port exclusion rule for tcp 1514 was found in semanage configuration." 
+                elif [ -n "$(echo "$semx_stat" | grep -F "$semx_none")" ]; then
+                    echo "INFO: port exclusion rule was not yet added to semanage configuration."
+                    echo "INFO: creating port exclusion rule using semanage, please wait..."
+                    sudo semanage port -a -t syslogd_port_t -p tcp 1514
+                else
+                    echo "ERROR: semanage port status returned unexpected data: $semx_stat"
+                fi
+            fi
+        else
+            echo "ERROR: SELinux is enabled but semanage status could not be determined. Contact your administrator."
+            exit 1
         fi
-        get_enforce
-    elif [ -n "$(which getenforce 2>&1)" ] && [ "$(sudo semanage port -a -t syslogd_port_t -p tcp 1514 2>&1)" = 'ValueError' ]; then
-        printf "INFO: SELinux is enabled and semanage is installed and syslogd tcp port 1514 has already been set by semanage.\nContinuing syslog configuration script..."
-    else
-       echo "ERROR: SELinux is enabled but semanage status could not be determined. Contact your administrator"    
-       exit 1
-    fi
-}
-
-get_enforce () {    
-    if [ "$(getenforce)" = "Permissive" ]; then
-        echo "INFO: getenforce reported Permissive SELinux configuration. Running semanage..."
-        sudo semanage port -a -t syslogd_port_t -p tcp 1514    
-    elif [ "$(getenforce)" = "Enforcing" ] && [ -n "$(command -v setenforce 2>&1)" ]; then
-        echo "getenforce reported Enforcing SELinux configuration. Toggling setenforce and running semanage..."
-        setenforce 0
-        sudo semanage port -a -t syslogd_port_t -p tcp 1514
-        setenforce 1 
     fi
 }
 
@@ -233,19 +301,23 @@ run_install () {
     check_enforce
     configure_agent
     make_syslog_config
-    if [ "$(sudo "$init_agent" status 2>&1)" = "al-agent is NOT running." ]; then
+    agent_svc_stat=$(sudo "$init_agent" status 2>&1)
+    if [ -n "$(echo "$agent_svc_stat" | grep -F "$als_no_run")" ]; then
         sudo "$init_agent" start
         if [ -n "$(pgrep al-agent)" ]; then
             echo "INFO: Agent service was started. Install complete."
         fi    
-    elif [ "$(sudo "$init_agent" status 2>&1)" = "al-agent is running" ]; then
-        echo "Agent service already running. Restarting..."
+    elif [ -n "$(echo "$agent_svc_stat" | grep -F "$als_is_run")" ]; then
+        echo "INFO: Agent service already running. Restarting..."
         sudo "$init_agent" restart     
         if [ -n "$(pgrep al-agent)" ]; then
-            echo "Agent service restarted. Install complete."
+            echo "Agent service was restarted. Install complete."
         fi    
-    else 
-        echo "Agent was installed but the service failed to start. Please check your system init and try again."
+    elif [ -n "$(echo "$agent_svc_stat" | grep -F "$err_no_file")" ]; then
+        echo "ERROR: Agent service file was not found at $init_agent. Verify package was installed"
+        exit 1
+    elif [ -n "$(echo "$agent_svc_stat" | grep -F "$als_no_cmd")" ]; then
+        echo "ERROR: Agent service command was not found. Check your permissions to the init folder $init_agent"
         exit 1
     fi
 }
@@ -280,3 +352,4 @@ else
     exit 1
 fi
 #END SCRIPT
+
